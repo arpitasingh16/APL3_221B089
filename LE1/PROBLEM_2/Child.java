2577ee908d8ca28a74625bf5953977d1ae52ebad
221B089 <221B089@juet.co.in>
2025-01-24 10:06:35 +0530

    Lab 1 commit

---
+++ LE1/PROBLEM_2/Child.java
@@ -0,0 +1,4 @@
+class Child extends Mother{
+    void show(){
+        System.out.println("hello JUET"); }
+}
---
+++ LE1/PROBLEM_3/Child.java
@@ -0,0 +1,4 @@
+class Child extends Mother{
+   static void show(){
+        System.out.println("hello JUET"); }
+}
